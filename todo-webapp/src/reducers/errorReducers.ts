9c99bfb357f488b1d231ef8bdb0d3cd606b60395
Daniel Nadar <daniel.nadar@vitra.ai>
2023-08-14 18:56:31 +0530

    updated the loading & error reducer states for Task

--- todo-webapp/src/reducers/errorReducers.ts
+++ todo-webapp/src/reducers/errorReducers.ts
@@ -1,16 +1,25 @@
-import { BOARDS } from "../constants";
+import { BOARDS, TASKS } from "../constants";
 
 const errorReducers = (state = null, action: any) => {
   switch (action.type) {
     case BOARDS.LOAD_FAIL:
+    case TASKS.LOAD_FAIL:
       return action.error;
+
     case BOARDS.LOAD:
     case BOARDS.LOAD_SUCCESS:
     case BOARDS.REQUEST_ADD_BOARD:
     case BOARDS.ADD_BOARD:
     case BOARDS.UPDATE_BOARD:
     case BOARDS.DELETE_BOARD:
+    case TASKS.REQUEST_ADD_TASK:
+    case TASKS.ADD_TASK:
+    case TASKS.REQUEST_DELETE_TASK:
+    case TASKS.DELETE_TASK:
+    case TASKS.REQUEST_UPDATE_TASK:
+    case TASKS.UPDATE_TASK:
       return null;
+
     default:
       return state;
   }
--- todo-webapp/src/reducers/loadingReducers.ts
+++ todo-webapp/src/reducers/loadingReducers.ts
@@ -1,12 +1,17 @@
-import { BOARDS } from "../constants";
+import { BOARDS, TASKS } from "../constants";
 
 const loadingReducers = (state = false, action: any) => {
   switch (action.type) {
     case BOARDS.LOAD:
+    case TASKS.LOAD:
       return true;
+
     case BOARDS.LOAD_SUCCESS:
     case BOARDS.LOAD_FAIL:
+    case TASKS.LOAD_SUCCESS:
+    case TASKS.LOAD_FAIL:
       return false;
+
     default:
       return state;
   }
